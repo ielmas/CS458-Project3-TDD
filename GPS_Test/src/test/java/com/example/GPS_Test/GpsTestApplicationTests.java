1b3c7cb93baa6d91588453bb761fa4e3ad51912c
kandogu <d.kose@ug.bilkent.edu.tr>
2021-04-13 20:54:01 +0300

    Add tryCity tests

--- GPS_Test/src/test/java/com/example/GPS_Test/GpsTestApplicationTests.java
+++ GPS_Test/src/test/java/com/example/GPS_Test/GpsTestApplicationTests.java
@@ -1,30 +1,67 @@
 package com.example.GPS_Test;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GpsTestApplicationTests {
 
+	public WebDriver webDriver;
+	public static String hostUrl = "";
+
+
 	@Before
 	void setup() {
 		// init driver
+		System.setProperty("webdriver.chrome.driver", "com/example/GPS_Test/chromedriver_windows/chromedriver.exe");
+		webDriver = new ChromeDriver();
+		webDriver.manage().window().maximize();
+		webDriver.get(hostUrl);
 	}
 
 	@After
-	void tearDown() {
+	void tearDown() throws InterruptedException {
        // quit driver
+		Thread.sleep(250L);
+		webDriver.quit();
 	}
 
 	@Test
-	void tryManualCoordinates() {
+	void tryManualCoordinates() throws InterruptedException {
+		tryCity("37.82", "32.59", "Konya");
+		tryCity("40.5", "35", "Çorum");
+		tryCity("38.7", "28.8", "Manisa");
+		tryCity("37.7", "27.1", "Aydın");
+		tryCity("38.43", "26.84", "İzmir");
+	}
 
+	public void tryCity(String latitude, String longitude, String expectedCity) throws InterruptedException {
+		webDriver.findElement(By.id("coordinates")).clear();
+		webDriver.findElement(By.id("coordinates")).sendKeys(latitude + ", " + longitude);
+		webDriver.findElement(By.id("sendCoordinatesBtn")).click();
+
+		Thread.sleep(100L);
+
+		Assert.assertEquals(webDriver.findElement(By.id("city")).getText(), expectedCity);
 	}
 
 	@Test
 	void tryInvalidCoordinates() {
+
+	}
+
+	public void tryInvalid(String latitude, String longitude, InvalidType invalidType) {
+		webDriver.findElement(By.id("coordinates")).clear();
+		webDriver.findElement(By.id("coordinates")).sendKeys(latitude, longitude);
+		webDriver.findElement(By.id("sendCoordinatesBtn")).click();
+
+
 
 	}
 
@@ -43,4 +80,7 @@ class GpsTestApplicationTests {
 	}
 
 
+	private enum InvalidType {
+		INVALID_LATITUDE, INVALID_LONGITUDE
+	}
 }
