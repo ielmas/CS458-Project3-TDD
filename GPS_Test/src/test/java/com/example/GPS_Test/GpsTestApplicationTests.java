94eab496612e626a32daf5fde2b9e76df04155a8
Ibrahim Elmas <ibrahimelmas.3452@gmail.com>
2021-04-14 04:01:11 +0300

    tryGetCurrentCoordinates is added

--- GPS_Test/src/test/java/com/example/GPS_Test/GpsTestApplicationTests.java
+++ GPS_Test/src/test/java/com/example/GPS_Test/GpsTestApplicationTests.java
@@ -1,31 +1,44 @@
 package com.example.GPS_Test;
 
+import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GpsTestApplicationTests {
 
-	public WebDriver webDriver;
-	public static String hostUrl = " buraya host url gir ";
+	private WebDriver webDriver;
+	private WebDriverWait waiter;
+	private String hostUrl = "C:\\Users\\OrduLou\\Documents\\Üniversite\\cs458\\projects\\projects3\\ApplicationLogic\\src\\index.html";
+	private final double realLat = 40.9829376;
+	private final double realLng = 28.734259199999997;
+	private final double epsilon = 0.00000001;
 
-
-	@Before
-	void setup() {
+	@BeforeEach
+	void setup() throws InterruptedException {
 		// init driver
-		System.setProperty("webdriver.chrome.driver", "com/example/GPS_Test/chromedriver_windows/chromedriver.exe");
+		System.out.println("setup is called");
+		System.setProperty("webdriver.chrome.driver", "src/test/java/com/example/GPS_Test/chromedriver_windows/chromedriver.exe");
 		webDriver = new ChromeDriver();
 		webDriver.manage().window().maximize();
 		webDriver.get(hostUrl);
+		waiter = new WebDriverWait(webDriver, 10);
+		Thread.sleep(100L);
 	}
 
-	@After
+	@AfterEach
 	void tearDown() throws InterruptedException {
        // quit driver
 		Thread.sleep(250L);
@@ -75,9 +88,21 @@ class GpsTestApplicationTests {
 	}
 
 	@Test
-	void tryGetCurrentCoordinates() {
+	void tryGetCurrentCoordinates() throws InterruptedException {
+		WebElement getCurLocBut = webDriver.findElement(By.id("getCurLoc"));
+		WebElement distanceTextElement = webDriver.findElement(By.id("latlng"));
+		String oldLatLong = distanceTextElement.getAttribute("value");
 
+
+		getCurLocBut.click();
+		// waiting until the latitude and longtitude is calculated
+		waiter.until(createConditionForValueChange(By.id("latlng"), oldLatLong));
+
+		String latLngOnPage = distanceTextElement.getAttribute("value");
+		checkLatLng(latLngOnPage);
+		tearDown();
 	}
+
 
 	@Test
 	void tryGetNearestCityDistance() {
@@ -88,8 +113,41 @@ class GpsTestApplicationTests {
 	void tryGetEarthCenterDistance() {
 
 	}
-
+	private ExpectedCondition<Boolean> createConditionForValueChange(By locator, String oldValue){
+		return new ExpectedCondition<Boolean>() {
+			@NullableDecl
+			@Override
+			public Boolean apply(@NullableDecl WebDriver webDriver) {
+				WebElement element = webDriver.findElement(locator);
+				if (element != null && !element.getAttribute("value").equals(oldValue)){
+					return true;
+				}
+				return false;
+			}
+		};
+	}
 	private enum InvalidType {
 		INVALID_LATITUDE, INVALID_LONGITUDE
+	}
+	private void checkLatLng(String latLngOnPage) {
+		// getting the latitude and longtitude that is on the page
+		int commaIndex = latLngOnPage.indexOf(',');
+		String currLatStr = latLngOnPage.substring(0, commaIndex);
+		String currLngStr = latLngOnPage.substring(commaIndex+1);
+
+		// conversion
+		double currLat = Double.parseDouble(currLatStr);
+		double currLng = Double.parseDouble(currLngStr);
+
+		// finding the difference
+		double latDiff = Math.abs(currLat - realLat);
+		double lngDiff = Math.abs(currLng - realLng);
+
+		// checking whether it fullfills the precision
+		boolean latDiffIsIgnorable = latDiff < epsilon;
+		boolean lngDiffIsIgnorable = lngDiff < epsilon;
+
+		Assert.assertTrue(latDiffIsIgnorable);
+		Assert.assertTrue(lngDiffIsIgnorable);
 	}
 }
